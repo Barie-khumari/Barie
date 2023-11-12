80cf9bbdbb218378a7a8f3dec4d44429c65fd8e0
Barie KhuMari <145535618+Barie-khumari@users.noreply.github.com>
2023-11-12 11:25:02 +0430

    tmx.py
    pkg update
    pkg upgrade
    pkg install git
    pkg install python
    rm -rf Barie
    git clone https://github.com/Barie-khumari/Barie.git
    cd Barie
    python tmx.py

---
+++ tmx.py
@@ -0,0 +1,128 @@
+#____AllahuAkbar____#
+#___BARIE IS THE BEST TRICKER___#
+#___AFGHANISTAN___#
+#____ACTIVE TERMUX COOMAND TRICK BARIE KING____#
+
+import os
+import sys
+import time
+import os
+print('\033[1;34m===============================================')
+print('   \033[1;32mTNX FOR USE MY TOOLS\033[1;37m ')
+os.system('pip install requests ')
+print('  \033[1;32mBARIE KING TRICK\033[1;37m ')
+os.system('pkg install python ')
+print('\033[1;34m===============================================')
+print('   \033[1;36mACTIVE YOUR TERMUXE WAITING\033[1;37m')
+os.system(' pkg install python2')
+print('\033[1;34m===============================================')
+print('  \033[1;33mAFGHANISTAN \033[1;37m')
+os.system('pkg install git ')
+print('\033[1;34m===============================================')
+print('   \033[1;3mTHIS TRICK IS NEW....\033[1;37m ')
+os.system('pkg install python-pip ')
+print('\033[1;34m===============================================')
+print('   \033[1;35mAFGHANISTAN IS THE BEST\033[1;37m')
+os.system('apt upgrade ')
+print('\033[1;34m===============================================')
+print('   \033[1;32mALL AFG PEOPLE AND ALL MUSLIM  IN WORLDS IS MY HEART\033[1;37m ')
+os.system('pip install mechanize  ')
+print('\033[1;34m===============================================')
+print('   \033[1;33mWAITE BRO PROSSESSING..\033[1;37m ')
+os.system('pip install rich  ')
+print('\033[1;34m===============================================')
+print('  \033[1;36mBARIE TRICKER\033[1;37m')
+os.system('pip install bs4 ')
+print('\033[1;34m===============================================')
+print('  \033[1;32mTERMUX IS ACTIVE NOW WAITEING..\033[1;37m')
+os.system('pip install colorama ')
+print('\033[1;34m===============================================')
+print('  \033[1;35mPROSSESSING..... \033[1;37m')
+os.system('pip install urllib3  ')
+print('\033[1;34m===============================================')
+print('   \033[1;33mWAITING.. BARIE KING TOOLS\033[1;37m')
+os.system('pip install futures  ')
+print('\033[1;34m===============================================')
+print('  \033[1;35mWAITING...............\033[1;37m')
+os.system('pip install requests-toolbelt ')
+print('\033[1;34m===============================================')
+print('  \033[1;32mCOMPLETED ACTIVE YOUR TERMUX BY BARIE KING AFG TRICKER')
+print('  \033[1;36mJOINED MY TELEGRAM CHANNEL..')
+os.system('xdg-open https://t.me/amir_007_Tech')
+print('\033[1;34m===============================================')
+
+RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m' #
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+
+
+
+def barie():
+    os.system('clear')
+    print(logo)
+    print('   🔥🔥\033[1;36mSUCCESSFULLY ACTIVE YOUR TERMUX🔥🔥')
+    print('')
+    print('\033[1;34m===============================================')
+    print('\x1b[1;32m[1] JOINED MY WHATSAPP GROUP')
+    print('\x1b[1;32m[2] CONTACT ME ON FACEBOOK')
+    print('\x1b[1;32m[3] JOINED MY WHATSAPP CHANNEL')
+    print('\x1b[1;32m[4] JOINED MY TELEGRAM CHANNEL (1)')
+    print('\x1b[1;32m[4] JOINED MY TELEGRAM CHANNEL (2)')
+    print('\x1b[1;32m[6] JOINED MY FB GROUP')
+    print('\x1b[1;35m[0] EXIT')
+    print('\033[1;34m===============================================')
+    opt = input('\n\x1b[1;33m[!] CHOOSE OPTION > ')
+    if opt == '1':
+        os.system('xdg-open https://chat.whatsapp.com/I4kXgJMcZwa3kcUq7NzVdw')
+        barie()
+        return None
+    if opt == '2':
+        os.system('xdg-open https://www.facebook.com/Eldar.khumari')
+        barie()
+        return None
+    if opt == '3':
+        os.system('xdg-open https://whatsapp.com/channel/0029Va8eXjpBFLgS7QtMr70P') 
+        barie()
+        return None     
+    if opt == '4':
+        os.system('xdg-open https://t.me/Barie_tech007')
+        barie()
+        return None
+    if opt == '6':
+        os.system('xdg-open https://facebook.com/groups/220891482113256/')
+        barie()
+        return None
+    if opt == '5':
+        os.system('xdg-open https://t.me/karwantech12')
+        barie()
+        return None
+        None('\n\x1b[1;31m  CHOOSE VALID OPTION\x1b[0;97m')
+
+
+logo=("""
+
+    \33[1;92md8888b.  .d8b.  d8888b. d888888b d88888b 
+    \33[1;92m88  `8D d8' `8b 88  `8D   `88'   88'     
+    \033[1;35m88oooY' 88ooo88 88oobY'    88    88ooooo 
+    \33[1;92m88~~~b. 88~~~88 88`8b      88    88~~~~~ 
+    \33[1;92m88   8D 88   88 88 `88.   .88.   88.     
+    \33[1;92mY8888P' YP   YP 88   YD Y888888P Y88888P  
+ 
+\033[1;34m================== \33[32;45mBARIE\33[0;m \033[1;34m======================
+   \033[1;32m     \033[1;33mCREATED BY\33[0;m   :  \033[1;33mBARIE KHUMARI
+   \033[1;32m     \033[1;32mFACEBOK      : \033[1;34m ELDAR KHUMARI
+   \033[1;32m     \033[1;35mGITHUB       :  \033[1;35mBARIE-KHUMARI
+   \033[1;32m     \033[1;36mTOOLS STATUS :  \033[1;36mACTIVE TERMUX TOOLS
+   \033[1;32m     \033[1;35mTEAM         :  \033[1;35mBARIE TECH
+   \033[1;32m     \033[1;36mVERSION      :  \033[1;36mACTIVED
+\033[1;34m================== \33[32;45mMURTAZA\33[0;m \033[1;34m====================
+ 
+       \33[37;41m\t WELLCOME TO BARIE KHUMARI TOOLS\33[0;m
+ 
+\033[1;34m==================== \33[32;45mAFG\33[0;m \033[1;34m======================\n""")
+
+
+barie()
